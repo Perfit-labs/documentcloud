3c41e71b870b1c0d04d70aabf28eb9e0a0b0287f
Ted Han <ted@knowtheory.net>
2016-02-29 06:47:25 -0800

    Noting down some scopes.

--- spec/models/organization_spec.rb
+++ spec/models/organization_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe Organization, type: :model do
-  it "should have an owner"
+  describe "ownership" do
+    it "should have an owner"
+  end
+  
+  describe "permissions for user actions" do
+    describe "document" do
+      context "upload" do
+      end
+      
+      context "editing" do
+      end
+    end
+    
+    describe "account" do
+      context "invitation" do
+      end
+      
+      context "editing" do
+      end
+    end
+  end
 end
